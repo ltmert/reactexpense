ea4b82a983dfe693e25863ee97a4f5d9b205fede
Mert <mert.gokkaya@gmail.com>
2019-11-06 16:17:05 -0500

    forms

--- expense/src/App.js
+++ expense/src/App.js
@@ -16,11 +16,49 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Expense Tracker
         </a>
       </header>
     </div>
+    
   );
+
+  function form () {
+    return (
+      <form>
+<label id="title">Expense Type
+  <div id="title">
+    <span>Expense_Type:</span> 
+      <select name="type" class="expensetype">
+          <option value="cashless">Cashless</option>
+          <option value="cash">Cash</option>
+        </select>
+  </div>
+  </label>
+  <label id="category"> Category
+  <select name="category" class="expensetype">
+                                    <option value="fixed">Fixed</option>
+                                    <option value="variable">Variable</option>
+                                    </select>
+                                    </label>
+ 
+   
+  <input type="text" name="subcategory" placeholder="subcategory" class="expensetype">
+  
+  <div>
+    <span>Date:</span> <input type="date" name="date" class="expensetype">
+  </div>
+  
+  <div>
+    <span>Amount:</span> <input type="number" name="amount" placeholder="$20" class="expensetype">
+  </div>
+  
+<form>
+</div>
+
+
+    )
+  }
 }
 
 export default App;
